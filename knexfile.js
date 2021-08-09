9bf9a3b9ee6bc432fa1b4b0d209ddc21dc2e97e9
Michael Chavez <mchavez@sd.marketing>
2021-08-09 16:34:05 -0700

    objectionjs

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+  development: {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: "USER",
+      password: "PASSWORD",
+      database: "DATABASE",
+      port: "8889",
+      dateStrings: true,
+    },
+  },
+};
